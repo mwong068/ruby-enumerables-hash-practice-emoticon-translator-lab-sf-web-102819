f8bc71b12141497d4bb44a336ccff4c08f0b5180
Megan Wong <mwong068@gmail.com>
2019-10-24 08:05:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,14 @@ require "yaml"
 def load_library(file_path)
  emoticons_library = YAML.load_file('./lib/emoticons.yml')
   
-  emoticons = {}
-  emoticons[get_meaning] = {}
-  emoticons[get_emoticon] = {}
+  emoticons = {
+    :get_meaning => {},
+    :get_emoticon => {}
+  }
   
   emoticons_library.each do |meaning, emoticons_array|
-      emoticons[get_meaning] = emoticons[get_meaning].merge({emoticons_array[1] => meaning})
-      emoticons[get_emoticon] = emoticons[get_emoticon].merge({emoticons_array[0] => emoticons_array[1]})
+      emoticons[:get_meaning] = emoticons[:get_meaning].merge({emoticons_array[1] => meaning})
+      emoticons[:get_emoticon] = emoticons[:get_emoticon].merge({emoticons_array[0] => emoticons_array[1]})
     end
 emoticons
 end
