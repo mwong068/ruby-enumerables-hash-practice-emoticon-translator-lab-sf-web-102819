66cff7fc8f48309a52f64f754ccb04f02e1bd307
Megan Wong <mwong068@gmail.com>
2019-10-24 07:53:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,19 @@
 require "yaml"
 
 def load_library(file_path)
- emoticons = YAML.load_file('./lib/emoticons.yml')
+ emoticons_library = YAML.load_file('./lib/emoticons.yml')
   
-  pp emoticons
+  emoticons = {}
+  emoticons[get_meaning] = {}
+  emoticons[get_emoticon] = {}
   
+  emoticons_library.each do |meaning, emoticons_array|
+    i = 0 
+    while i < emoticons_array.length
+      emoticons[get_meaning][emoticons_array[i+1]] = {}
+    i += 2
+    end
+  end
  
-
+ pp emoticons
 end
